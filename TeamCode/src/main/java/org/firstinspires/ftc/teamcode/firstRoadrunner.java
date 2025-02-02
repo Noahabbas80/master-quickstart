2d9e3a418fb78ebd416ceaa312ec08d781811bdd
Noahabbas80 <121987786+Noahabbas80@users.noreply.github.com>
2025-02-01 21:35:19 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstRoadrunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstRoadrunner.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name="firstRoadrunner")
 public class firstRoadrunner extends LinearOpMode {
@@ -17,8 +19,26 @@ public class firstRoadrunner extends LinearOpMode {
 
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(0,0,0))
-                        .lineToX(64)
+                        .lineToX(32)
+                        .stopAndAdd(new ServoAction(armServo, .8))
+                        .WaitSeconds(.3)
                         .lineToX(0)
                         .build());
+    }
+
+    public class ServoAction() implements Action{
+
+
+        Servo servo;
+        double pos;
+        public ServoAction(Servo servo, double pos){
+            this.servo = servo;
+            this.pos = pos;
+        }
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket){
+            servo.setPosition(pos);
+            return false;
+        }
     }
 }
