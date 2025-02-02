71a9bbf3368b59ca19980969a001c4f7d9aae937
Noahabbas80 <121987786+Noahabbas80@users.noreply.github.com>
2025-02-01 23:57:11 -0500

    ,

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,35 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, -55, Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(-50, -50, Math.toRadians(45)))
+                        .lineToLinearHeading(new Pose2d(-47, -45, Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(-50, -50, Math.toRadians(45)))
+                        .lineToLinearHeading(new Pose2d(-57, -45, Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(-50, -50, Math.toRadians(45)))
+                        .lineToLinearHeading(new Pose2d(-62, -45, Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(-50, -50, Math.toRadians(45)))
+                        .splineTo(new Vector2d(60, -60), Math.toRadians(0))
+                        .build());
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
