b872097765c4c7a35f09942367b46cc08eb90be9
Benjamin Bernadskiy <bbernadskiy1224@gmail.com>
2025-10-19 00:11:38 -0400

    temp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SDItest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SDItest.java
@@ -28,39 +28,39 @@ public class SDItest extends LinearOpMode {
         bl = hardwareMap.dcMotor.get("bl");
         fr = hardwareMap.dcMotor.get("fr");
         br = hardwareMap.dcMotor.get("br");
-        sr = hardwareMap.dcMotor.get("sr");
-        sl = hardwareMap.dcMotor.get("sl");
+//        sr = hardwareMap.dcMotor.get("sr");
+//        sl = hardwareMap.dcMotor.get("sl");
 
-        linkServo = hardwareMap.servo.get("linkServo");
-        workServo = hardwareMap.servo.get("workServo");
-        headServo = hardwareMap.servo.get("headServo");
-        frontClawServo = hardwareMap.servo.get("frontClawServo");
-        backClawServo = hardwareMap.servo.get("backClawServo");
-        armServo = hardwareMap.servo.get("armServo");
-
-        sr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        sl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        sr.setTargetPosition(0);
-        sl.setTargetPosition(0);
-        frontClawServo.setPosition(1);
-        backClawServo.setPosition(1);
-
-
-        sr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        sl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        sl.setDirection(DcMotor.Direction.REVERSE);
+//        linkServo = hardwareMap.servo.get("linkServo");
+//        workServo = hardwareMap.servo.get("workServo");
+//        headServo = hardwareMap.servo.get("headServo");
+//        frontClawServo = hardwareMap.servo.get("frontClawServo");
+//        backClawServo = hardwareMap.servo.get("backClawServo");
+//        armServo = hardwareMap.servo.get("armServo");
+//
+//        sr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        sl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//
+//        sr.setTargetPosition(0);
+//        sl.setTargetPosition(0);
+//        frontClawServo.setPosition(1);
+//        backClawServo.setPosition(1);
+//
+//
+//        sr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        sl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//
+//        sl.setDirection(DcMotor.Direction.REVERSE);
 
         fr.setDirection(DcMotor.Direction.REVERSE);
         br.setDirection(DcMotor.Direction.REVERSE);
         fl.setDirection(DcMotor.Direction.REVERSE);
         bl.setDirection(DcMotor.Direction.REVERSE);
 
-        sr.setPower(.95);
-        sl.setPower(.95);
-
-        linkServo.setPosition(0.98);
+//        sr.setPower(.95);
+//        sl.setPower(.95);
+//
+//        linkServo.setPosition(0.98);
 
         waitForStart();
 
@@ -70,7 +70,7 @@ public class SDItest extends LinearOpMode {
             currentGamepad2.copy(gamepad1);
 
             p1Controls();
-            p2Controls(currentGamepad2, previousGamepad2);
+            //p2Controls(currentGamepad2, previousGamepad2);
 
             telem();
 
@@ -99,36 +99,40 @@ public class SDItest extends LinearOpMode {
 
     public void p2Controls(Gamepad currentGamepad2, Gamepad previousGamepad2) {
 
-        linkServo.setPosition(gamepad2.right_trigger * -.3 + .63);
-        workServo.setPosition(headUp ? .6 : .29 );
-
-
-        if(currentGamepad2.left_bumper && !previousGamepad2.left_bumper){
-            headUp = !headUp;
-        }
-        if(currentGamepad2.right_bumper && !previousGamepad2.right_bumper){
-            slideUp = !slideUp;
-        }
-        if(slideUp){
-            sl.setTargetPosition(2100);
-            sr.setTargetPosition(2100);
-        }
-        else{
-            sl.setTargetPosition(15);
-            sr.setTargetPosition(15);
-        }
+//        linkServo.setPosition(gamepad2.right_trigger * -.3 + .63);
+//        workServo.setPosition(headUp ? .6 : .29 );
+//
+//
+//        if(currentGamepad2.left_bumper && !previousGamepad2.left_bumper){
+//            headUp = !headUp;
+//        }
+//        if(currentGamepad2.right_bumper && !previousGamepad2.right_bumper){
+//            slideUp = !slideUp;
+//        }
+//        if(slideUp){
+//            sl.setTargetPosition(2100);
+//            sr.setTargetPosition(2100);
+//        }
+//        else{
+//            sl.setTargetPosition(15);
+//            sr.setTargetPosition(15);
+//        }
     }
 
 
     public void telem() {
-        telemetry.addData("elm pos", armServo.getPosition());
-        telemetry.addData("back claw", linkServo.getPosition());
-        telemetry.addData("head claw", headServo.getPosition());
+//        telemetry.addData("elm pos", armServo.getPosition());
+//        telemetry.addData("back claw", linkServo.getPosition());
+//        telemetry.addData("head claw", headServo.getPosition());
+        telemetry.addData("fr", fr.getCurrentPosition());
+        telemetry.addData("fl", fl.getCurrentPosition());
+        telemetry.addData("br", br.getCurrentPosition());
+        telemetry.addData("bl", bl.getCurrentPosition());
 
-        telemetry.addData("work claw", workServo.getPosition());
-
-        telemetry.addData("SR pos", sr.getCurrentPosition());
-        telemetry.addData("Sl pos", sl.getCurrentPosition());
+//        telemetry.addData("work claw", workServo.getPosition());
+//
+//        telemetry.addData("SR pos", sr.getCurrentPosition());
+//        telemetry.addData("Sl pos", sl.getCurrentPosition());
         telemetry.update();
     }
 
